0eed615522cd48a2a36ba71a996fa8d9d4342b73
la218256 <la218256@student.helha.be>
2024-07-28 01:30:18 +0200

    Itinial commit

---
+++ src/main/java/be/helha/poo3/exsp/paniers/PaniersApplication.java
@@ -0,0 +1,13 @@
+package be.helha.poo3.exsp.paniers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaniersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaniersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/helha/poo3/exsp/paniers/PaniersApplicationTests.java
@@ -0,0 +1,13 @@
+package be.helha.poo3.exsp.paniers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaniersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
