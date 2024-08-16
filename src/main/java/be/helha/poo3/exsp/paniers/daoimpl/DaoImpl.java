adc46d34aafeb5d441bc20b49b1c4bc18a71ec2c
la218256 <la218256@student.helha.be>
2024-08-16 09:31:56 +0200

    Ajout Classe DaoImpl

---
+++ src/main/java/be/helha/poo3/exsp/paniers/daoimpl/DaoImpl.java
@@ -0,0 +1,13 @@
+package be.helha.poo3.exsp.paniers.daoimpl;
+
+import be.helha.poo3.exsp.paniers.dao.Dao;
+
+/**
+ * La classe DaoImpl est une implémentation basique de l'interface Dao.
+ * Actuellement, cette classe ne contient aucune logique ou méthode spécifique.
+ */
+public class DaoImpl implements Dao {
+    // À l'heure actuelle, cette classe n'ajoute aucune fonctionnalité supplémentaire.
+    // Elle est essentiellement un marqueur ou un point d'extension pour des futures implémentations.
+}
+
