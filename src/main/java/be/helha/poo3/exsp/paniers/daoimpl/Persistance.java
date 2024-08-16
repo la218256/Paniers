f9a5ef08f14599fcc5f1387243a2689fd1f3f3ae
la218256 <la218256@student.helha.be>
2024-08-16 09:12:57 +0200

    Ajout methode getDaoImpl()

--- src/main/java/be/helha/poo3/exsp/paniers/daoimpl/Persistance.java
+++ src/main/java/be/helha/poo3/exsp/paniers/daoimpl/Persistance.java
@@ -22,4 +22,38 @@ public class Persistance implements Dao {
         super();
     }
 
+    /**
+     * Retourne une implémentation de DAO en fonction de l'interface DAO passée en paramètre.
+     *
+     * @param interfaceDao L'interface DAO pour laquelle une implémentation est requise.
+     * @return Une instance de l'implémentation du DAO, ou null si aucune implémentation n'est trouvée.
+     */
+    public Dao getDaoImpl(Class<? extends Dao> interfaceDao) {
+        Dao dao = null;
+        Class<Dao> classeDaoImpl;
+        Class<?>[] interfaces;
+
+        try {
+            // Parcourt les noms des DAO disponibles
+            for (String nomDaoImpl : daos) {
+                // Charge la classe DAO correspondante à partir de son nom
+                classeDaoImpl = (Class<Dao>) Class.forName(nomDaoImpl);
+                interfaces = (Class<?>[]) classeDaoImpl.getInterfaces();
+
+                // Vérifie si la classe implémente l'interface demandée
+                for (Class<?> i : interfaces) {
+                    if (i.getName().equals(interfaceDao.getName())) {
+                        // Crée une instance de l'implémentation du DAO
+                        dao = (Dao) classeDaoImpl.getDeclaredConstructor().newInstance();
+                        return dao;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
 }
