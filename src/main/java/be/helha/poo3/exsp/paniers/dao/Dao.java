82e00b06074da797a4cda62cdb699697a8bef847
la218256 <la218256@student.helha.be>
2024-08-16 08:59:23 +0200

    Ajout Interface Dao

---
+++ src/main/java/be/helha/poo3/exsp/paniers/dao/Dao.java
@@ -0,0 +1,13 @@
+package be.helha.poo3.exsp.paniers.dao;
+
+/**
+ * Interface de marquage pour les classes DAO.
+ *
+ * Cette interface est utilisée pour catégoriser toutes les classes
+ * qui interagissent avec la base de données dans l'application. Bien
+ * qu'elle ne définisse aucune méthode, elle peut être utilisée pour
+ * identifier les classes DAO et leur fournir des fonctionnalités
+ * communes via l'héritage multiple ou des vérifications de type.
+ */
+public interface Dao {
+}
