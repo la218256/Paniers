ad436a9b5a398d37dfe2d85d3672b4d7e5bd1b39
la218256 <la218256@student.helha.be>
2024-08-16 10:07:21 +0200

    Ajout Classe RervationDaoImpl + Requete + Constructeur privee

---
+++ src/main/java/be/helha/poo3/exsp/paniers/daoimpl/ReservationDaoImpl.java
@@ -0,0 +1,23 @@
+package be.helha.poo3.exsp.paniers.daoimpl;
+
+import be.helha.poo3.exsp.paniers.dao.ReservationDao;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ReservationDaoImpl implements ReservationDao {
+    // Requêtes SQL utilisées dans les méthodes
+    private static final String GET = "SELECT * FROM reservations WHERE id = ?";
+    private static final String AJOUT = "INSERT INTO reservations (date_commande, quantite, prix, client_id) VALUES (?,?,?,?)";
+    private static final String MAJ = "UPDATE reservations SET date_commande = ?, quantite = ?, prix = ?, client_id = ? WHERE id = ?";
+    private static final String LISTER = "SELECT * FROM reservations ORDER BY reservations.id";
+    private static final String SUPPRIMER = "DELETE FROM reservations WHERE id = ?";
+
+    // Instance de la fabrique DAO pour obtenir les connexions et autres DAO
+    private DaoFactory daoFactory;
+
+    // Constructeur qui initialise la fabrique DAO
+    public ReservationDaoImpl() {
+        this.daoFactory = DaoFactory.getInstance();
+    }
+
+}
